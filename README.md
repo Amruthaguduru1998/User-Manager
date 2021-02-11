9b7a5b224875a2eec3a44ef78e161d28c2b39d95
Sahil A Sheikh <sahilasheikh123@gmail.com>
2021-02-11 17:03:28 +0530

    model class and DAO added

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-## Employee Manager
+## JSP CRUD
 * This is a JSP Web Application that performs CRUD [Create, Retrieve, Update & Delete] operation on Oracle Database.
 
 ## Getting Started
 * You just have to download the file and import in any java/jdbc editor.
 
 ## Prerequisites
-* You need install any JAVA editor, Oracle & Apache Tomcat and have to create table [create table emp (eno number(10), ename varchar2(20), salary number(99);]
+* You need install any JAVA editor, Oracle & Apache Tomcat and have to create table (create table crud_user(sr_no number(10), name varchar2(2000), email varchar2(2000), password varchar2(2000), mobile_number number(10));) and run this application.
 
 ## Built With
 * This application is developed using Java, JDBC, Oracle.
---
+++ src/com/jspcrud/dao/Employee_DAO.java
@@ -0,0 +1,111 @@
+package com.jspcrud.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jspcrud.model.Employee;
+
+public class Employee_DAO {
+	
+//	method to get JDBC connection
+	protected static Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "admin");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+//	method to insert the employee's data into table
+	public static int insert(Employee emp) {
+		int i = 0;
+		try {
+			Connection connection = Employee_DAO.getConnection();
+			PreparedStatement ps = connection.prepareStatement("insert into emp values(?,?,?");
+			ps.setInt(1, emp.getEno());
+			ps.setString(2, emp.getEname());
+			ps.setDouble(3, emp.getSalary());
+			i = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return i;
+	}
+	
+//	method to retrieve the employee's data from table by eno
+	public static Employee retrieve(int eno) {
+		Employee emp = null;
+		try {
+			Connection connection = Employee_DAO.getConnection();
+			PreparedStatement ps = connection.prepareStatement("select * from emp where eno = ?");
+			ps.setInt(1, eno);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				emp.setEno(rs.getInt(1));
+				emp.setEname(rs.getString(2));
+				emp.setEname(rs.getString(3));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return emp;
+	}
+	
+//	method to update the emoloyee's data into table
+	public static int update(Employee emp) {
+		int i = 0;
+		try {
+			Connection connection = Employee_DAO.getConnection();
+			PreparedStatement ps = connection.prepareStatement("update emp set ename = ?, salary = ? where eno = ?");
+			ps.setString(1, emp.getEname());
+			ps.setDouble(2, emp.getSalary());
+			ps.setInt(3, emp.getEno());
+			i = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return i;
+	}
+	
+//	method to delete the employee's data from table
+	public static int delete(Employee emp) {
+		int i = 0;
+		try {
+			Connection connection = Employee_DAO.getConnection();
+			PreparedStatement ps = connection.prepareStatement("delete emp where eno = ?");
+			ps.setInt(1, emp.getEno());
+			i = ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return i;
+	}
+	
+//	method to retrieve the complete employee's table
+	public static List<Employee> retrirvetable() {
+		List<Employee> employees = new ArrayList<Employee>();
+		try {
+			Connection connection = Employee_DAO.getConnection();
+			PreparedStatement ps = connection.prepareStatement("select * from emp");
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				Employee employee = new Employee();
+				employee.setEno(rs.getInt(1));
+				employee.setEname(rs.getString(1));
+				employee.setSalary(rs.getDouble(3));
+				employees.add(employee);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return employees;
+	}
+		
+}
